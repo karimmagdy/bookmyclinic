c7ff55c80b60add7f3b69d21bb1c784142a09ef8
Karim Magdy <karimagdy22@gmail.com>
2026-06-11 01:57:24 +0300

    Stage 1 — Add missing tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,12 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
-
-export default config;
+}
+export default config
